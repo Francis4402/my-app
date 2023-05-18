c4789c2eb02c708af74f8d6d630f390e853dc2a0
DESKTOP-EGP26JE\Admin <francisconio25@gmail.com>
2023-05-18 21:56:43 +0600

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
-        <div>
+        <div className=" relative">
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Francis</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm:block hidden" />interfaces and web applications</p>
         </div>
